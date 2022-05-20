2eb66b5d1f034627f5290261da1662fe8d7a4976
amin <105122483+aminnjfi5157@users.noreply.github.com>
2022-05-20 19:07:51 +0430

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+x =float(input("please enter first number :"))
+y = float(input("please enter socond number :"))
+print("the answer is : "+str(x/y))
+c = x/y
+c = str(c)
+print(c[0:4])
+print(c[0:10:2])
+z = "mohammadAmin"
+q = "najafi"
+r = "engineer"
+h = 4326
+print("user %s %s %s %d is programmer" %(z,q,r,h))
+
